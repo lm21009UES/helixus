76bc34015bca43cf64487c00495b232038991bb4
= <=>
2023-04-21 17:36:59 -0600

    Agregando interactividad para cada sub-página

--- assets/styles/change-content.js
+++ assets/styles/change-content.js
@@ -1,8 +1,12 @@
 // Seleccionamos los enlaces y el div de contenido
 var home = document.getElementById("home");
 var buttons = document.getElementById("buttons");
-var enlace2 = document.getElementById("enlace2");
-var enlace3 = document.getElementById("enlace3");
+var forms = document.getElementById("formularies");
+var cards = document.getElementById("cards");
+var alerts = document.getElementById("alerts");
+var navbars = document.getElementById("navbars");
+var badges_shadows = document.getElementById("badges-shadows");
+var containers = document.getElementById("containers");
 var destiny = document.getElementById("content");
 
 // Agregamos un evento de clic a cada enlace
@@ -14,12 +18,28 @@ buttons.addEventListener("click", function() {
     cargarContenido("./assets/www/buttons.html");
 });
 
-enlace2.addEventListener("click", function() {
-    cargarContenido("OtraPagina.html");
+forms.addEventListener("click", function() {
+    cargarContenido("./assets/www/formularies.html");
 });
 
-enlace3.addEventListener("click", function() {
-    cargarContenido("OtraPaginaMas.html");
+cards.addEventListener("click", function() {
+    cargarContenido("./assets/www/cards.html");
+});
+
+alerts.addEventListener("click", function() {
+    cargarContenido("./assets/www/alerts.html");
+});
+
+navbars.addEventListener("click", function() {
+    cargarContenido("./assets/www/navbars.html");
+});
+
+badges_shadows.addEventListener("click", function() {
+    cargarContenido("./assets/www/badges-shadows.html");
+});
+
+containers.addEventListener("click", function() {
+    cargarContenido("assets/www/containers.html");
 });
 
 // Función para cargar el contenido de la página HTML y reemplazar el contenido del div
